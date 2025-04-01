e89b998c59d9eb5e0265159142fa48bdbd34a756
yourshemaa <hemajavemulapalli@gmail.com>
2025-04-01 15:06:40 +0530

    initial commit of django project

---
+++ demo/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from . models import Product
+def product_list(request):
+  products = Product.objects.all()
+  return render(request, 'demo/product_list.html', {'products': products})
